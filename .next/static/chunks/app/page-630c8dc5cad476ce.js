d3ebc0dd5207ed7dc632f733f53e9bfeaf3c47db
Sarah-coder05 <149367867+Sarah-coder05@users.noreply.github.com>
2024-12-02 19:55:48 +0100

    Restore repository and push mistakenly pulled code

---
+++ .next/static/chunks/app/page-630c8dc5cad476ce.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931,883,91,258,97,455,336],{7412:function(e,n,s){Promise.resolve().then(s.bind(s,3017)),Promise.resolve().then(s.t.bind(s,8173,23))}},function(e){e.O(0,[231,173,17,971,23,744],function(){return e(e.s=7412)}),_N_E=e.O()}]);
---
+++ index.js
@@ -0,0 +1,11 @@
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <h1>Hello, Trade Connect!</h1>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
